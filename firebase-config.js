97feb2d8d7a171b01876d6b693dac099ed0d121e
GarfieldApp <garfieldapp@outlook.com>
2025-03-03 09:25:20 +0100

    first publish

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// Your web app's Firebase configuration
+// For Firebase JavaScript SDK v7.20.0 and later, measureID is optional
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "garfieldapp-4c237",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
